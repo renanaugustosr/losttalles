fef70dc49243afe7465ba4dcd3cf3bd9170799b7
renan augusto <renanaugustosr@gmail.com>
2020-09-24 17:50:50 -0300

    a

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 //rotas
-require('./app/routes/note.routes.js')(app);
+require('./app/routes/jogador.routes.js')(app);
 
 // define a simple route
 app.get('/', (req, res) => {
